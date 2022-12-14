781507ad98a6742afd9ad3b63697eee5b76449e5
SchrodingersMouser <113191739+SchrodingersMouser@users.noreply.github.com>
2022-12-14 14:52:19 +0000

    code

--- CodespacesProject/ProsProject_Consolidated/src/main.cpp
+++ CodespacesProject/ProsProject_Consolidated/src/main.cpp
@@ -79,21 +79,21 @@ void usercontrol()
                    controller.get_analog(ANALOG_LEFT_Y),
                    controller.get_analog(ANALOG_LEFT_X));
  
-        if (controller2.getDigital(ControllerDigital::L1))
+        if (controller2.getDigital(L1))
             flywheelOn();
-        else if (controller2.getDigital(ControllerDigital::L2))
+        else if (controller2.getDigital(L2))
             flywheelOut();
         else
             flywheelOff();
  
-        if (controller2.getDigital(ControllerDigital::R1))
+        if (controller2.getDigital(R1))
             intakeIn();
-        else if (controller2.getDigital(ControllerDigital::R2))
+        else if (controller2.getDigital(R2))
             intakeOut();
         else
             intakeOff();
  
-        if (def::controller2.getDigital(ControllerDigital::down))
+        if (controller2.getDigital(down))
             air.toggle();
         else
             mtr_move.move_voltage(0);
@@ -106,7 +106,7 @@ void usercontrol()
  * When this callback is fired, it will toggle line 2 of the LCD text between
  * "I was pressed!" and nothing.
  */
-void on_center_button()
+/* void on_center_button()
 {
     static bool pressed = false;
     pressed = !pressed;
@@ -118,7 +118,7 @@ void on_center_button()
     {
         pros::lcd::clear_line(2);
     }
-}
+} */
  
 /**
  * Runs initialization code. This occurs as soon as the program is started.
@@ -128,10 +128,9 @@ void on_center_button()
  */
 void initialize()
 {
-    pros::lcd::initialize();
-    pros::lcd::set_text(1, "Hello PROS User!");
- 
-    pros::lcd::register_btn1_cb(on_center_button);
+	lcd.register_btn1_cb(on_center_button);
+	lcd.initialize();
+	lcd.set_text(1,"Hello PROS User!");
 }
  
 /**
@@ -180,22 +179,23 @@ void autonomous() {}
  */
 void opcontrol()
 {
-    pros::Controller master(pros::E_CONTROLLER_MASTER);
-    pros::Motor left_mtr(1);
-    pros::Motor right_mtr(2);
+    Controller master(E_CONTROLLER_MASTER);
+    Motor left_mtr(1);
+    Motor right_mtr(2);
  
     while (true)
     {
-        pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-                         (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-                         (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
+        lcd.print(0, "%d %d %d", (lcd.read_buttons() & LCD_BTN_LEFT) >> 2,
+                         (lcd.read_buttons() & LCD_BTN_CENTER) >> 1,
+                         (lcd.read_buttons() & LCD_BTN_RIGHT) >> 0);
+		
         int left = master.get_analog(ANALOG_LEFT_Y);
         int right = master.get_analog(ANALOG_RIGHT_Y);
  
         left_mtr = left;
         right_mtr = right;
  
-        pros::delay(20);
+        delay(20);
     }
 }
  
