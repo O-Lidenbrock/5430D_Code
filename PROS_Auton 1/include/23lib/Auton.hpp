d8f92e4095eed950137d8b952d01281c209e62b3
Father Time <113191739+SchrodingersMouser@users.noreply.github.com>
2023-01-27 15:31:28 -0600

    auton should work now

---
+++ PROS_Auton 1/include/23lib/Auton.hpp
@@ -0,0 +1,17 @@
+#pragma once 
+
+#include <iostream> 
+
+class Auton
+{
+    public: 
+        static void autoncontrol();
+
+        /*static void autonMiddle(double,double,double); 
+        static void autonMove(); 
+        static void intakeOff(); 
+        */
+        
+       // static void flywheelOut(); 
+       // static void flywheelOff(); 
+};
