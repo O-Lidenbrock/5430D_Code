6292af857ac796e91fb2f3e20427d9ec7be08ef5
Father Time <113191739+SchrodingersMouser@users.noreply.github.com>
2023-02-10 12:31:19 -0600

    Stuff

--- CallbackDrive/src/main.cpp
+++ CallbackDrive/src/main.cpp
@@ -8,7 +8,7 @@ int main() {
 void piston(){
 }
 
-void move(double leftFront, double leftBack, double rightFront,
+void move(double leftFront, double leftBack, double rightFront, //flatten these numbers with the curve here: https://www.desmos.com/calculator/rerhnlaim4
           double rightBack){     
     mtr_dt_left_front.setVelocity(leftFront, percent); // define the motors
     mtr_dt_left_back.setVelocity(leftBack, percent);
@@ -19,6 +19,12 @@ void move(double leftFront, double leftBack, double rightFront,
 void moveArcade(double forward, double strafe, double turn){
     move(forward + strafe + turn, forward - strafe + turn,
        forward - strafe - turn, forward + strafe - turn); //order matters
+}
+
+void userDrive(){
+       moveArcade(controller1.getAnalog(rightY),
+               controller1.getAnalog(rightX),
+               controller1.getAnalog(leftX)/turnDivisor);
 }
 
 void intakeIn(int speed){
